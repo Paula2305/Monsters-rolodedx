4543bbc20edd473d070f5d59676cf3551160b557
Paula Moreno <paulasolmoreno@gmail.com>
2024-09-02 00:47:54 -0300

    Migracion de clases a componentes funcionales

--- src/App.js
+++ src/App.js
@@ -1,67 +1,111 @@
-import { Component } from 'react';
+// import { Component } from 'react'; al no utilizar mas componentes de clase lo eliminamos
 
-import './App.css';
+import { useState, useEffect } from 'react';
 
 import SearchBox from './components/search-box/search-box.component';
 import CardList from './components/card-list/card-list.component';
+import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: ''
-    };
-  }
+const App = () => {
 
-  // Cuando se tenga una clase de componente que necesita hacer una llamada a una API para obtener datos que se necesita mostrar ene la UI, se requiere poner en el componenteDidMount()
-  componentDidMount() {
-    console.log('componentDidMount')
+  const [searchField, setSearchField] = useState(''); // [value, set] value = valor que queremos almacenar, set = el que setea nuestra funcion
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  // La unica vez que se tiene qque llamar a esta funcion es cuando se ejecuta por prrimera vezz
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        ));
-  }
+    .then((response) => response.json())
+    .then((users) => setMonsters(users));
+  }, []); // (callback = efecto que queuremos q suceda, array de dependencias)
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(() => {
-      return { searchField };
-    });
-  }
-
-  // render() determina qué mostrar. Monta la UI een el DOM
-  // Los componentes se renderizan cada vez que:
-  // - cuando se llama al setState
-  // - cuando se actualizan los props
-  render() {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) => { // Se crea un array nuevo "filteredMonsters" que contiene los monstruos filtrados sin modificar el array original 
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => { 
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
-    return (
-      <div className="App">
-        <SearchBox
-          className='monsters-search-box'
-          onChangeHandler={onSearchChange} 
-          placeholder='search monsters'  />
+    setFilteredMonsters(newFilteredMonsters);
 
-        {/* Los componentes creados se deben escribir con Capitalize */}
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
+  }, [monsters,searchField]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+
+  return (
+    <div className="App">
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBox
+        className='monsters-search-box'
+        onChangeHandler={onSearchChange} 
+        placeholder='search monsters'  />
+      <CardList monsters={filteredMonsters} /> 
+    </div>
+  );
 
 }
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//       searchField: ''
+//     };
+//   }
+
+//   // Cuando se tenga una clase de componente que necesita hacer una llamada a una API para obtener datos que se necesita mostrar ene la UI, se requiere poner en el componenteDidMount()
+//   componentDidMount() {
+//     console.log('componentDidMount')
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(
+//           () => {
+//             return { monsters: users };
+//           },
+//           () => {
+//             console.log(this.state);
+//           }
+//         ));
+//   }
+
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLocaleLowerCase();
+//     this.setState(() => {
+//       return { searchField };
+//     });
+//   }
+
+//   // render() determina qué mostrar. Monta la UI een el DOM
+//   // Los componentes se renderizan cada vez que:
+//   // - cuando se llama al setState
+//   // - cuando se actualizan los props
+//   render() {
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+
+//     const filteredMonsters = monsters.filter((monster) => { // Se crea un array nuevo "filteredMonsters" que contiene los monstruos filtrados sin modificar el array original 
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+
+//     return (
+//       <div className="App">
+        
+//         <SearchBox
+//           className='monsters-search-box'
+//           onChangeHandler={onSearchChange} 
+//           placeholder='search monsters'  />
+
+//         {/* Los componentes creados se deben escribir con Capitalize */}
+//         <h1 className='app-title'>Monsters Rolodex</h1>
+//         <CardList monsters={filteredMonsters} />
+//       </div>
+//     );
+//   }
+
+// }
 
 export default App;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,8 +3,7 @@ import './card.styles.css'
 
 class Card extends Component{
     render(){
-        const {monster} = this.props;
-        const {name, email, id} = monster;
+        const {name, email, id} = this.props.monster;
         return (
             <div className="card-container" key={id}>
                 <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
