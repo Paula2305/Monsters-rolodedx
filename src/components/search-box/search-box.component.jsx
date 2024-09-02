1d804d2ff535aae0f523ccf2c63fe6a64a80a29d
Paula Moreno <paulasolmoreno@gmail.com>
2024-09-02 12:49:49 -0300

    Migracion de todos los componentes de clases a componentes funcionales

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,17 +1,14 @@
-import { Component } from "react";
-import './search-box.styles.css'
-class SearchBox extends Component {
-    render() {
 
-        return (
-            <input
-                className={`search-box ${this.props.className}`} // interpolación
-                type='search'
-                placeholder= {this.props.placeholder}
-                onChange={this.props.onChangeHandler}
-            />
-        )
-    }
+import './search-box.styles.css'
+const SearchBox = (props) => {
+    return (
+        <input
+            className={`search-box ${props.className}`} // interpolación
+            type='search'
+            placeholder= {props.placeholder}
+            onChange={props.onChangeHandler}
+        />
+    )
 }
 
 export default SearchBox;
